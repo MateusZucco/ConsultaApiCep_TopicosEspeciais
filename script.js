849b9bab4cc0d649a7c0f1d06348ede41f6a97d2
Mateus Zucco <mateuszuccco@gmail.com>
2022-06-01 21:12:50 -0300

    Adjusts

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ button.addEventListener("click", findCEP)
 async function findCEP(){ 
     var cep = document.getElementById("cep")
     try{
-        const response = await fetch(`https://viacep.com.br/ws/${cep.value}/json`)
+        const formattedCep = cep.value.replace(/[^0-9]/g, '')
+        const response = await fetch(`https://viacep.com.br/ws/${formattedCep}/json`)
         const data = await response.json()
-        console.log(data)
         print(data)
         return data   
     }catch(error){
